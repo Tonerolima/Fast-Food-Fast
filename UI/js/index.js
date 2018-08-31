9b93f412d4bc063d619c80361883e25dd6a7fc7f
Anthony Oyathelemhi <tonero91@gmail.com>
2018-08-31 12:51:33 +0100

    change fontawesome hamburger icon to unicode character

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,16 +1,14 @@
-const toggler = document.getElementById('nav-toggle');
+const toggler = document.getElementById('nav-button');
 const linksWrapper = document.querySelector('.nav-links-wrapper');
 const nav = document.querySelector('#navbar');
 
 const openMenu = () => {
-    toggler.classList.remove('fa-bars');
-    toggler.classList.add('fa-times', 'white');
+    toggler.classList.add('white');
     linksWrapper.classList.add('animate');
 }
 
 const closeMenu = () => {
-    toggler.classList.remove('fa-times', 'white');
-    toggler.classList.add('fa-bars');
+    toggler.classList.remove('white');
     linksWrapper.classList.remove('animate');
 }
 
