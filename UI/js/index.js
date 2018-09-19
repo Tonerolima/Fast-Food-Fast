90847feb65d6c01683ba8761908cabcba0c8a7c5
Anthony Oyathelemhi <tonero91@gmail.com>
2018-09-19 05:48:24 +0100

    Add login, register and logout by saving token to localStorage

--- UI/js/index.js
+++ UI/js/index.js
@@ -14,6 +14,45 @@ const closeMenu = () => {
     linksWrapper.classList.remove('animate');
 }
 
+const checkToken = () => {
+  if (localStorage.getItem('authToken')) {
+    document.querySelector('.nav-links:nth-of-type(2)').innerHTML = '<li id="logout"><a href="#">Logout</a></li>';
+    document.getElementById('logout').addEventListener('click', (event) => {
+      logout();
+    })
+  }
+}
+
+const logout = () => {
+  localStorage.removeItem('authToken');
+  window.location = 'file:///C:/Users/toner/webdev/UI/Fast-Food-Fast/UI/login.html';
+}
+
+const login = (data) => {
+  const XHR = new XMLHttpRequest();
+
+  const formData = new FormData(data);
+
+  // Define what happens on successful data submission
+  XHR.addEventListener("load", (event) => {
+    let response = JSON.parse(event.target.responseText);
+    if (!response.status) { return alert(response.message); }
+    localStorage.setItem('authToken', response.token);
+    window.location = 'file:///C:/Users/toner/webdev/UI/Fast-Food-Fast/UI/index.html';
+  });
+
+  // Define what happens in case of error
+  XHR.addEventListener("error", (event) => {
+    alert('Oops! Something went wrong.');
+  });
+
+  // Set up our request
+  XHR.open(data.method, data.action);
+
+  // The data sent is what the user provided in the form
+  XHR.send(formData);
+}
+
 toggler.addEventListener('click', function () {
     if (!linksWrapper.classList.contains('animate')) {
         openMenu();
@@ -21,3 +60,5 @@ toggler.addEventListener('click', function () {
         closeMenu();
     }
 })
+
+checkToken();
