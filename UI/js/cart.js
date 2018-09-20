87590809b0bf8b2a7f3ad0ff47d9173e914ff4a4
Anthony Oyathelemhi <Tonerolima@users.noreply.github.com>
2018-09-20 14:05:31 +0100

    Ui templates (#62)
    * responsive navbar design
    
    * landing page setup
    
    * create views for login and sign up
    
    * add background images to login and signup pages
    
    * change the transition property to transform only
    
    * add viewport meta tag for better veiwing on mobile devices
    
    * add viewport tag to login and sign up pages
    
    * design admin page for orders management
    
    * define hidden class and new media query for < 700px screens
    
    * add order history page
    
    * menu page design
    
    * change button colors
    
    * design cart page and redesign the look of the cards
    
    * remove redundant styles and add new classes for better modulation
    
    * change classnames to restore responsiveness, remove redundant css files and refactor navbar toggle
    
    * adjustments for <500px screens
    
    * replace nav links with correct addresses
    
    * fix padding on nv links
    
    * add side menu
    
    * switch menu to left
    
    * change food names and images
    
    * Fix nav container margin
    
    * create classes for card details and define font-size
    
    * Apply google fonts
    
    * Change default body font
    
    * make links take up the size of their container
    
    * Change logo position
    
    * make vertical card 100% on small screens
    
    * create small container class
    
    * add pointer cursor to nav-toggle
    
    * Add search on homescreen, remove border-radius, change landing page bg
    
    * fix filter function
    
    * set doctype and charset declarations
    
    * fix minor display gimmicks on the section classes
    
    * fetch menu with AJAX
    
    * XMLHttpRequest setup to submit form data asynchronously
    
    * Add login, register and logout by saving token to localStorage
    
    * save cart to localStorge
    
    * refactor cart functions
    
    * add interractivity to cart view
    
    * send formData with foodId and qty on checkout
    
    * Set image object-fit property to cover
    
    * Add unicode naira character
    
    * make deleted cart item animate
    
    * add navbar close button

---
+++ UI/js/cart.js
@@ -0,0 +1,79 @@
+const list = document.querySelector('.vertical.list');
+const total = document.getElementById('total');
+const checkoutButton = document.getElementById('checkout');
+
+document.onreadystatechange = () => {
+  const cart = retrieveCart();
+  if (document.readyState === "complete") {
+    let sum = 0;
+    let node = '';
+    cart.forEach((item, key) => {
+      sum += parseInt(item.cost);
+      node += `
+      <li class="horizontal card">
+        <div class="img-thumbnail">
+          <img src="${item.image}" alt="${item.name}">
+        </div>
+        <div class="horizontal card-details">
+          <h4 class="food-name">${item.name}</h4>
+          <h6 class="amount">Amount: &#8358 <span class="amount">${item.cost}</span></h6>
+          <h6>Qty: 1</h6>
+          <div class="order-buttons">
+            <button class="big button decline" data-id="${key}" data-cost="${item.cost}">Remove</button>
+          </div>
+        </div>
+      </li>
+      `
+    });
+    list.innerHTML = node;
+    total.textContent = sum;
+  }
+}
+
+const fadeOut = (element) => {
+  element.classList.add('fade');
+  window.setTimeout(() => {
+    element.classList.add('shrink');
+  }, 600)
+  window.setTimeout(() => {
+    element.classList.add('hidden');
+  }, 1100)
+}
+
+list.addEventListener('click', (event) => {
+  const clicked = event.target;
+  if (clicked.tagName === 'BUTTON') {
+    removeFromCart(clicked.dataset.id);
+    total.textContent = parseInt(total.textContent) - parseInt(clicked.dataset.cost);
+    const parentList = clicked.parentNode.parentNode.parentNode;
+    fadeOut(parentList);
+    // parentList.removeChild(clicked.parentNode.parentNode.parentNode);
+  }
+});
+
+checkoutButton.addEventListener('click', (event) => {
+  const cart = retrieveCart();
+  checkout(cart);
+})
+
+const checkout = (cart) => {
+  const obj = new FormData();
+  cart.forEach((value, key) => {
+    obj.append(key, value.qty);
+  })
+  
+  const XHR = new XMLHttpRequest();
+
+  XHR.addEventListener("load", (event) => {
+    let response = JSON.parse(event.target.responseText);
+    console.log(response);
+    // if (event.target.status === 201) { 
+    //   console.log(response.result);
+    // }
+
+  });
+
+  XHR.open('POST', 'http://localhost:8080/api/v1/orders');
+  XHR.setRequestHeader('Authorization', `Bearer ${localStorage.authToken}`);
+  XHR.send(obj);
+}
---
+++ UI/js/index.js
@@ -0,0 +1,121 @@
+const openToggler = document.getElementById('nav-toggle-open');
+const closeToggler = document.getElementById('nav-toggle-close');
+const linksWrapper = document.querySelector('.nav-links-wrapper');
+const nav = document.querySelector('#navbar');
+
+const openMenu = () => {
+    // toggler.classList.remove('fa-bars');
+    // toggler.classList.add('fa-times', 'white');
+    linksWrapper.classList.add('animate');
+}
+
+const closeMenu = () => {
+    // toggler.classList.remove('fa-times', 'white');
+    // toggler.classList.add('fa-bars');
+    linksWrapper.classList.remove('animate');
+}
+
+const checkToken = () => {
+  if (localStorage.authToken) {
+    return true;
+  }
+  return false;
+}
+
+const toggleAuthLinks = () => {
+  const navLinks = document.querySelector('.nav-links:nth-of-type(2)');
+  if (checkToken()) {
+    navLinks.innerHTML = '<li id="logout"><a href="#">Logout</a></li>';
+    document.getElementById('logout').addEventListener('click', (event) => {
+      logout();
+    })
+  }
+}
+
+const createCart = () => {
+  if (!localStorage.cart) {
+    localStorage.cart = '[]';
+    console.log(localStorage.cart);
+  }
+}
+
+const retrieveCart = () => {
+  return new Map(JSON.parse(localStorage.cart));
+}
+
+const resaveCart = (array) => {
+  return localStorage.cart = JSON.stringify(Array.from(array.entries()));
+}
+
+const addToCart = async (food) => {
+  if (!food) { return false }
+  const { id } = food;
+  const properties = (({ name, image, cost }) => ({ name, image, cost }))(food);
+  properties.qty = 1;
+  const cart = retrieveCart();
+  await cart.set(id, properties);
+  return resaveCart(cart);
+}
+
+const removeFromCart = async (foodId) => {
+  if (!foodId) { return false }
+  const cart = retrieveCart();
+  await cart.delete(foodId);
+  return resaveCart(cart);
+}
+
+const checkCartForItem = (foodId) => {
+  if (!foodId) { return false }
+  return retrieveCart().has(foodId);
+}
+
+const logout = () => {
+  localStorage.removeItem('authToken');
+  window.location = 'file:///C:/Users/toner/webdev/UI/Fast-Food-Fast/UI/login.html';
+}
+
+const login = (data) => {
+  const XHR = new XMLHttpRequest();
+
+  const formData = new FormData(data);
+
+  // Define what happens on successful data submission
+  XHR.addEventListener("load", (event) => {
+    let response = JSON.parse(event.target.responseText);
+    if (!response.status) { return alert(response.message); }
+    localStorage.setItem('authToken', response.token);
+    window.location = 'file:///C:/Users/toner/webdev/UI/Fast-Food-Fast/UI/index.html';
+  });
+
+  // Define what happens in case of error
+  XHR.addEventListener("error", (event) => {
+    alert('Oops! Something went wrong.');
+  });
+
+  // Set up our request
+  XHR.open(data.method, data.action);
+
+  // The data sent is what the user provided in the form
+  XHR.send(formData);
+}
+
+openToggler.addEventListener('click', function () {
+  openMenu();
+    // if (!linksWrapper.classList.contains('animate')) {
+    //     openMenu();
+    // } else {
+    //     closeMenu();
+    // }
+})
+
+closeToggler.addEventListener('click', function () {
+  closeMenu();
+    // if (!linksWrapper.classList.contains('animate')) {
+    //     openMenu();
+    // } else {
+    //     closeMenu();
+    // }
+})
+
+toggleAuthLinks();
+createCart();
