113958de7a06c620fbcc1216ad3d64f8733966d2
Anthony Oyathelemhi <tonero91@gmail.com>
2018-09-25 08:15:16 +0100

    chore-replace-lib-with-server-in-test-file-#160748029

--- test/routes_test.js
+++ test/routes_test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../lib/app').default;
+const app = require('../server/app').default;
 
 const { assert } = chai;
 const { expect } = chai;
