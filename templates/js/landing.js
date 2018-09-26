cf33399be68ff3785a0726f78c08f2c8538c188b
Anthony Oyathelemhi <tonero91@gmail.com>
2018-09-26 07:26:55 +0100

    Replace search box with details of the service the app offers
    This includes a welcome message and slogan. A new section comes below with
    a list of menu items that are fetched asynchronously from the server

--- templates/js/landing.js
+++ templates/js/landing.js
@@ -1,3 +1,20 @@
+const list = document.querySelector('.horizontal.list');
+const arrow = document.querySelector('.fa-angle-down');
+
+fetchMenu(8, 0, list);
+
+let movedDownIntervalId = setInterval(() => {
+  arrow.classList.add('moved-down');
+  return setTimeout(() => {
+    arrow.classList.remove('moved-down');
+  }, 500)
+}, 2000);
+
 window.onscroll = () => {
-  this.scrollY <= 100 ? nav.className = 'landing' : nav.className = '';
+  if (this.scrollY <= 100) { 
+    nav.className = 'landing';
+  } else {
+    nav.className = '';
+  }
+  window.clearInterval(movedDownIntervalId);
 };
