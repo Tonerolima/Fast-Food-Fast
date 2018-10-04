fe6ec890562d0ddc248184d15cee0696a38baf60
Anthony Oyathelemhi <tonero91@gmail.com>
2018-10-04 02:10:22 +0000

    set additional response headers to avoid preflight requests

--- server/app.js
+++ server/app.js
@@ -26,6 +26,9 @@ app.use(methodOverride('_method'));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Max-Age', '1728000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
