67631da9efe28943aa63907c6f045f1ff389bcbc
Anthony Oyathelemhi <tonero91@gmail.com>
2018-10-04 00:10:39 +0000

    Add 'authorization' to allowed headers

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(methodOverride('_method'));
 // Enable cross-origin resource sharing
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
