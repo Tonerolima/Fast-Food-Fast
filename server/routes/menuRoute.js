037b9b71b88c5d700f8e035cd456cd5ae41621a5
Anthony Oyathelemhi <tonero91@gmail.com>
2018-10-10 17:38:09 +0000

    Add GET menu/foodId route and getMenuItem controller function

--- server/routes/menuRoute.js
+++ server/routes/menuRoute.js
@@ -2,13 +2,14 @@ import express from 'express';
 import middleware from '../middleware';
 import Menu from '../controllers/menuController';
 
-const { verifyToken, validateMenuItem } = middleware;
+const { verifyToken, validateMenuItem, validateParams } = middleware;
 
 const router = express();
 const v1 = express.Router();
 
 v1.post('/', verifyToken, validateMenuItem, Menu.addFood);
 v1.get('/', Menu.getMenu);
+v1.get('/:foodId', validateParams, Menu.getMenuItem);
 
 
 router.use('/api/v1/menu', v1);
